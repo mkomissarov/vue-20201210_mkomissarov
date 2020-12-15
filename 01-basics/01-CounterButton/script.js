083cbc3cf6ecc14e1f96805b76b866422eb32953
Komissarov, Mikhail <m.komissarov@x5.ru>
2020-12-15 13:11:28 +0300

    01-basic counter done

--- 01-basics/01-CounterButton/script.js
+++ 01-basics/01-CounterButton/script.js
@@ -2,3 +2,18 @@ import Vue from './vue.esm.browser.js';
 
 // const app = ...
 // Рекомендуется использовать МЕТОД в качестве обработчика события
+
+const app = new Vue({
+  el: '#app',
+
+  data() {
+    return {
+      counter: 0,
+    };
+  },
+  methods: {
+    increaseCounter() {
+      return (this.counter += 1);
+    },
+  },
+});
