7f2ce04f3ba44654c69efe892ca7decf21559182
Komissarov, Mikhail <m.komissarov@x5.ru>
2020-12-22 10:13:56 +0300

    02-components button counter

--- 02-components/01-CounterButtonComponent/CounterButton.js
+++ 02-components/01-CounterButtonComponent/CounterButton.js
@@ -1,10 +1,31 @@
 export const CounterButton = {
   // Шаблон потребуется отредактировать
-  template: '<button type="button"></button>',
+  template: '<button type="button"  @click="increaseCounter">{{count}}</button>',
 
   // Компонент должен иметь входной параметр
+  props: {
+    count: {
+      type: Number,
+      default: 0,
+    },
+  },
 
   // Компонент должен иметь модель
+  model: {
+    prop: 'count',
+    event: 'increment',
+  },
 
   // Шаблон лучше держать максимально простым, а логику выносить в методы
+  methods: {
+    increaseCounter() {
+      this.$emit('increment', this.count + 1);
+    },
+  },
+
+  beforeMount: () => {
+    if (typeof this.count !== 'number' || this.count === 0) {
+      this.$emit('increment', 0);
+    }
+  },
 };
